cdd915b67d190daf5ffceba52dc8aa9fd4f6255d
M Kunde <mikolaj.kundegorski@gmail.com>
2017-03-24 01:32:02 +0300

    Specified a translation task

---
+++ 4_translator.py
@@ -0,0 +1,25 @@
+"""
+This program translates words between English-French-Malagasy using a dictionary specified in a JSON file.
+
+1. Execute file and see how it tranlsates a word "salama"
+2. Create a loop allowing to translate more words. You can make a condition that loop exits if user writes word "veloma".
+
+3. Allow translations between three languages. It will require a different approach. My initial idea is to specify corresponding lists of words for every language:
+'en-fr-ma.json'
+
+Good luck!
+"""
+
+import json
+
+with open('ma-en.json') as json_data:
+    ma_en_dict = json.load(json_data)
+
+ma_word = input("Word in Malagasy: ")
+en_word = ma_en_dict[ma_word]
+print ("En: ", en_word)
+
+print("Veloma! Goodbye!")
+
+
+
