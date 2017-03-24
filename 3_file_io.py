7c1dba4f4bbe8dd8842c7e30893ece6f67fa97ae
Zoely <zralamboarisoa@gmail.com>
2017-03-24 11:14:52 +0300

    It is not diffucult

--- 3_file_io.py
+++ 3_file_io.py
@@ -15,8 +15,18 @@ meerkat_file = open('meerkats.txt', 'r')
 
 for line in meerkat_file:
     words = line.strip().split()
-    for word in words:
-        print(word)
-    print(line)
+    #for word in words:
+	
+print(line)
 
 meerkat_file.close()
+
+last_meerkat_file = open('last_meerkats.txt', 'w')
+last_meerkat_file.write(line)
+last_meerkat_file.close()
+
+last_meerkat_file = open('last_meerkats.txt', 'r')
+for line in last_meerkat_file:
+    print("the line in the last file",line)
+
+
